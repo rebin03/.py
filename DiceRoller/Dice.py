22ca8e35220c31ff3a429800b89b77b5f842a40b
rebin03 <muhammedrebin1794@gmail.com>
2023-05-30 18:35:42 +0530

    DiceRoller

---
+++ DiceRoller/Dice.py
@@ -0,0 +1,53 @@
+import random
+while True:
+    print('--- WELCOME TO DICE ROLLER ---\n')
+    print('1.Roll the dice\n2.Exit')
+    user = int(input("what you want to do(1/2)\n"))
+    
+    if user == 1:
+        number = random.randint(1,6)
+        # print(number)
+        if number == 1:
+            print('\nYour dice number is 1')
+            print(' -----')
+            print('|     |')
+            print('|  •  |')
+            print('|     |')
+            print(' -----')
+        elif number == 2:
+            print('\nYour dice number is 2')
+            print(' -----')
+            print('|  •  |')
+            print('|     |')
+            print('|  •  |')
+            print(' -----')
+        elif number == 3:
+            print('\nYour dice number is 3')
+            print(' -----')
+            print('|  •  |')
+            print('|  •  |')
+            print('|  •  |')
+            print(' -----')
+        elif number == 4:
+            print('\nYour dice number is 4')
+            print(' -----')
+            print('| • • |')
+            print('|     |')
+            print('| • • |')
+            print(' -----')
+        elif number == 5:
+            print('\nYour dice number is 5')
+            print(' -----')
+            print('| • • |')
+            print('|  •  |')
+            print('| • • |')
+            print(' -----')
+        else:
+            print('\nYour dice number is 6')
+            print(' -----')
+            print('| • • |')
+            print('| • • |')
+            print('| • • |')
+            print(' -----')
+    else:
+        break
