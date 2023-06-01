ae98b68ac6bf3605909feeff0fdbb3bf4a44e1b3
rebin03 <muhammedrebin1794@gmail.com>
2023-06-02 01:46:09 +0530

    Added limit for Guessing

--- NumberGuessing/NumGuessGame.py
+++ NumberGuessing/NumGuessGame.py
@@ -3,19 +3,25 @@ import random
 x=random.randint(1,100)
 
 print('---NUMBER GUESSING GAME---\n')
-print('You can guess a nummber between 1 and 100\nYou can guess untill you find the number!!')
+print('You can guess a nummber between 1 and 100\nYou have only 10 chances to guess the number!!')
 
 guess = int(input("Guess a number: "))
+count = 1
 
 if x == guess:
     print('Wow!! You guessed the number right in the first try.')
 else:
-    while x != guess:
+    while x != guess and count<10:
+        count += 1
         if x > guess:
             print('You guessed a smaller number\n')
         else:
             print('You guessed a larger number\n')
         guess = int(input("Guess a number: "))
-    print('You guessed the number right!!')
-print('The number was: ', x)
+    if x == guess:
+        print('\nYou guessed the number right!!')
+    else:
+        print('\nYou lost the game!!')
+        print('The number was: ', x)
+
 print('\n---END---')
